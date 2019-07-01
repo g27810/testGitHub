ac5149ff386d0307eda2d82b19553c90c37f704e
g27810 <g2781062630@126.com>
2019-07-01 18:23:06 +0800

    测试GitHub

---
+++ helloGitHub/src/com/itheima/demo/TestClass.java
@@ -0,0 +1,13 @@
+package com.itheima.demo;
+
+/**
+ * @author 自由的信仰
+ *  ClassName: TestClass
+ *  date 2019-07-01 18:12:46
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        //上传GitHub第二个测试
+        System.out.println("学习要一步一步来,不要畏难,积跬步致千里");
+    }
+}
